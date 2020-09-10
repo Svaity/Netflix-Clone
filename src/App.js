2a481383bb45c7d87a7a388f99e65031e024260d
Shrey Vaity <43662680+Svaity@users.noreply.github.com>
2020-09-10 03:07:22 -0400

    React App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Row from './Row'
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <h1>Hey Lets Build Netflix Frontend</h1>
+      <Row title="Netflix ORIGINALS"/>
+      <Row title="Trending Now"/>
     </div>
   );
 }
---
+++ src/requests.js
@@ -0,0 +1,10 @@
+const API_KEY = "ebf446afaa484295170650af09888706";
+
+const requests = {
+    fetchTrending: "/trending/all/week?api_keys=${API_KEY}&language=en-US"
+    // fetchNetFlixOriginals: ''
+}
+
+
+
+export default requests;
