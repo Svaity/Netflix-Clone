4c14fb80972e3a3fcfebdd0fec0d89e47f7677b8
Shrey Vaity <43662680+Svaity@users.noreply.github.com>
2020-11-01 21:10:48 -0500

    Project complete Frontend

--- src/requests.js
+++ src/requests.js
@@ -1,10 +1,14 @@
-const APIKEY = "ebf446afaa484295170650af09888706";
+const API_KEY = 'Your TMDB Api_Key';
 
 const requests = {
-    fetchTrending: '/trending/all/week?api_key=ebf446afaa484295170650af09888706&language=en-US',
-    fetchNetflixOriginals: '/discover/tv?api_key=ebf446afaa484295170650af09888706&with_networks=123'
-}
-
-
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
 
 export default requests;
